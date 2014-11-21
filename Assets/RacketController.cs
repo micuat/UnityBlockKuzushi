958497f886762c989a30b1c1423a7c0e5d83cc20
naoto <micuat@gmail.com>
2014-11-21 17:22:53 -0500

    Receive osc and apply to racket; bug that UnityOSC timestamp is broken with oF so currently append timestamp to address by sender

--- Assets/RacketController.cs
+++ Assets/RacketController.cs
@@ -1,19 +1,38 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityOSC;
 
 public class RacketController : MonoBehaviour {
 
 	private float Accel = 1000.0f;
+    private Dictionary<string, ServerLog> servers;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        OSCHandler.Instance.Init(); //init OSC
+        servers = new Dictionary<string, ServerLog>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		rigidbody.AddForce(transform.right * Input.GetAxisRaw("Horizontal") * Accel, ForceMode.Impulse);
-	}
+
+        OSCHandler.Instance.UpdateLogs();
+        servers = OSCHandler.Instance.Servers;
+
+        foreach (KeyValuePair<string, ServerLog> item in servers)
+        {
+            // If we have received at least one packet,
+            // show the last received from the log in the Debug console
+            foreach(OSCPacket packet in item.Value.packets)
+            {
+                rigidbody.AddForce(-transform.right * (float)packet.Data[1] * Accel * 0.02f, ForceMode.Impulse);
+            }
+        }
+    }
 
 	private float tolerance = 0.5f;
 	
